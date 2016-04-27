a46911625ef5a2e13042c57be036c491a09c3bd2
rsimsek-tgm <rsimsek@student.tgm.ac.at>
2016-04-27 06:46:38 +0200

    tried fixing imports again, still having issues

--- csvApp/test.py
+++ csvApp/test.py
@@ -1,42 +1,32 @@
 import unittest
-import CSVApp
+from csvApp.csvApp import CSVApplication
 
-__author__="Raphael Simsek"
-__date__="2016-04-21"
-__status__="deployed"
+__author__ = "Raphael Simsek"
+__date__ = "2016-04-21"
+__status__ = "deployed"
 
 class CSVtest(unittest.TestCase):
     def setUp(self):
-        self.c = CSVApp.CSVApplication('test.csv')
+        self.c = CSVApplication('test.csv')
 
     def test_load1(self):
-        self.csvClass= CSVApp.CSVApplication('test.csv')
+        self.csvClass= CSVApplication('test.csv')
         self.csvClass.csvreader()
-        self.assertTrue()
+        self.assertTrue(None)
 
     def test_load2(self):
         try:
-            self.csvClass = CSVApp.CSVApplication('test1.csv')
-            self.csvClass.read()
+            self.csvClass = CSVApplication('test1.csv')
+            self.csvClass.csvreader()
         except IOError as e:
             self.assertTrue("CSV-file couldn't be loaded\n %s" % e)
 
     def test_load3(self):
         try:
-            self.csvClass = CSVApp.CSVApplication('test.csv')
+            self.csvClass = CSVApplication('test.csv')
             self.csvClass.getdialect()
         except AssertionError as e:
             self.assertFalse("CSV dialect could not be found\n %s" % e)
 
 if __name__ == '__main__':
     unittest.main()
-
-    csvClass = CSVApplication('../test.csv')
-    # csvClass.getdialect()
-    csvClass.csvreader()
-    #time.sleep(5)
-    csvClass.csvwriter()
-    #time.sleep(5)
-    csvClass.csvwriteradd()
-    #time.sleep(10)
-    csvClass.csvaddtodata()
